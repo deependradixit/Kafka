d0d1f2f75f625bd409cbc5eadb0cda1b98160943
deependra.dixit <deependra.dixit@cloudsmartz.net>
2017-03-21 10:56:29 +0530

    commit

--- Kafka/src/main/java/org/bitbucket/kafka/KafkaController.java
+++ Kafka/src/main/java/org/bitbucket/kafka/KafkaController.java
@@ -21,7 +21,7 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder;
 public class KafkaController {
 	private static final Logger logger = LoggerFactory.getLogger(KafkaController.class);
 	
-	// hello testing in kafka555555
+	// hello testing in kafka4444
 	
 	//line1
 	
