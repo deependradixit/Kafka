d5e40673e353a6232983236f256948e8c52568b3
Deependra Dixit <deependradixit12@gmail.com>
2017-03-21 10:23:13 +0530

    Update KafkaController.java

--- Kafka/src/main/java/org/bitbucket/kafka/KafkaController.java
+++ Kafka/src/main/java/org/bitbucket/kafka/KafkaController.java
@@ -22,7 +22,7 @@ public class KafkaController {
 	private static final Logger logger = LoggerFactory.getLogger(KafkaController.class);
 	
 	// hello testing in kafka555555
-	//read topic
+	//read topic name
 	
 	
 	
